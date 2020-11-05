0fc34c2bab1420baa55c8c5b83fca29188814313
unknown <tdiep.hou@gmail.com>
2020-11-05 12:08:34 -0600

    dev branch commit

---
+++ zerodraft/questionnaire/main.js
@@ -0,0 +1,12 @@
+/*
+  Copyright (c) 2020 - present, DITDOT Ltd. - MIT Licence
+  https://github.com/ditdot-dev/vue-flow-form
+  https://www.ditdot.hr/en
+*/
+
+import Vue from 'vue'
+import Rebate from './rebate.vue'
+
+new Vue({
+  render: h => h(Rebate)
+}).$mount('#app')
